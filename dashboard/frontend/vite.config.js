064954b40db520748c7a3d4339b6f77df8d76c87
pratham15541 <prathamparikh94@gmail.com>
2026-02-27 18:50:13 +0530

    fourth commit

--- dashboard/frontend/vite.config.js
+++ dashboard/frontend/vite.config.js
@@ -3,15 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 
 const proxyTarget = process.env.VITE_PROXY_TARGET || "http://127.0.0.1:8000";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
+    host: true, // 🔥 important
     proxy: {
       "/api": {
         target: proxyTarget,
         changeOrigin: true,
       },
     },
+  },
+
+  preview: {
+    host: true, // 🔥 important
+    allowedHosts: "all", // 🔥 allows ngrok domains
   },
 });
